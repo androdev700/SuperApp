7f5d1a158cc3304f06be068886c3c31ef0c2c2b0
rahulranjan <genirahul@gmail.com>
2019-06-21 19:00:10 +0530

    Minor updates

--- app/src/main/java/com/oldmen/superapp/ui/fragment/discovery/Data.java
+++ app/src/main/java/com/oldmen/superapp/ui/fragment/discovery/Data.java
@@ -2,12 +2,14 @@ package com.oldmen.superapp.ui.fragment.discovery;
 
 public class Data {
 
-    public static final String discoveryString = "[{\"title\":\"Pratilipi Hackathon\",\"category\":\"Update\",\"description\":\"Know whats going on in the pratilipi hackathon\", \"members\":40, \"logo\":\"\"}," +
+    public static final String discoveryString = "[{\"title\":\"Pratilipi Hackathon\",\"category\":\"Update\",\"description\":\"Know whats going on in the pratilipi hackathon\", \"members\":40, \"logo\":\"https://github.com/androdev700/SuperApp/raw/master/logos/pratilipi.png\"}," +
             "{\"title\":\"Unacademy\",\"category\":\"Education\",\"description\":\"India's largest learning platform\", \"members\":400, \"logo\":\"https://github.com/androdev700/SuperApp/raw/master/logos/unacademy.jpg\"}," +
             "{\"title\":\"Netflix\",\"category\":\"Entertainment\",\"description\":\"Watch anywhere\",\"rating\":4, \"members\":832, \"logo\":\"https://github.com/androdev700/SuperApp/raw/master/logos/Netflix_icon.png\"}," +
             "{\"title\":\"Zomato\",\"category\":\"Food\",\"description\":\"Find the best restaurants, cafés, and bars in Bengaluru\",\"rating\":4, \"members\":900, \"logo\":\"https://github.com/androdev700/SuperApp/raw/master/logos/zomato.png\"}," +
             "{\"title\":\"Makemytrip\",\"category\":\"Food\",\"description\":\"Book flights and hotels for your dream destination\",\"rating\":4, \"members\":900, \"logo\":\"https://github.com/androdev700/SuperApp/raw/master/logos/makemytrip.jpg\"}," +
             "{\"title\":\"Economic Times\",\"category\":\"News\",\"description\":\"Stay updated about what happening around the world\",\"rating\":4, \"members\":900, \"logo\":\"https://github.com/androdev700/SuperApp/raw/master/logos/economics-time.png\"}," +
             "{\"title\":\"Angry Birds\",\"category\":\"Game\",\"description\":\"Find the best restaurants, cafés, and bars in Bengaluru\",\"rating\":4, \"members\":900, \"logo\":\"https://github.com/androdev700/SuperApp/raw/master/logos/angry_birds.jpg\"}," +
+            "{\"title\":\"Flipkart\",\"category\":\"Shopping\",\"description\":\"Wide Range of Genuine Products, Easy Returns, Cash on Delivery, Browse Now! Hassle Free Shopping. EMI Option Available. Free & Easy Returns. Enjoy Delightful Prices. Types: Smartphones, Electronics, Home Appliances, Apparel, Books.\",\"rating\":4, \"members\":900, \"logo\":\"https://github.com/androdev700/SuperApp/raw/master/logos/flipkart.png\"}," +
+            "{\"title\":\"Spotify\",\"category\":\"Shopping\",\"description\":\"Listen to Your Favourite Artists, Songs and Albums for Free. Sign Up Today! Listen on Several Devices. Discover Great New Music. Find Your Favorite Albums. Play Music for Free.\",\"rating\":4, \"members\":900, \"logo\":\"https://github.com/androdev700/SuperApp/raw/master/logos/spotify.png\"}," +
             "{\"title\":\"Coursera\",\"category\":\"Education\",\"description\":\"100% online learning from the world’s best universities and companies\",\"rating\":4, \"members\":400, \"logo\":\"https://github.com/androdev700/SuperApp/raw/master/logos/coursera.png\"}]";
 }
